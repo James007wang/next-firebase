c69dcfc80b34c2bb0bcac2bf8559bc252acbb4a8
James007wang <daoxue.wang@teknion.com>
2024-11-13 15:11:57 -0500

    add config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -8,12 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // Attention Please add your firebase console config here
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBFBrwtnWSlBsVyn9JkUPQ8Kv-qCVnAV9A",
+  authDomain: "balmy-platform-423519-j4.firebaseapp.com",
+  projectId: "balmy-platform-423519-j4",
+  storageBucket: "balmy-platform-423519-j4.firebasestorage.app",
+  messagingSenderId: "825233968869",
+  appId: "1:825233968869:web:bbc4c065c3c28b08254e0c",
+  measurementId: "G-H9ST35XH3D"
 };
 
 // Initialize Firebase
